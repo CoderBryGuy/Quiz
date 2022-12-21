601feb25dedb0452a73c1612647346607dff6d71
DESKTOP-RPN6QGT <vryan8822@gmail.com>
2022-12-21 22:52:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/quiz/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.example.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+
+    ImageView image;
+    TextView title;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        image = findViewById(R.id.imageView_splash);
+        title = findViewById(R.id.textView_splash);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplication(),R.anim.splash_anim);
+        title.startAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+
+    }
+}
